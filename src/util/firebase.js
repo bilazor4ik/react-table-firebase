3a48da91aa896d48ca0f00bb6f3c00130c78a024
AlexB <bilazor@gmail.com>
2022-02-16 17:25:51 -0500

    Add firebase credentials

---
+++ src/util/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const config = {
+  apiKey: "AIzaSyDXgsj6mypw9dHkeI2X1bI1LM9ayC-wYQo",
+  authDomain: "deliveries-status.firebaseapp.com",
+  databaseURL: "https://deliveries-status-default-rtdb.firebaseio.com",
+  projectId: "deliveries-status",
+  storageBucket: "deliveries-status.appspot.com",
+  messagingSenderId: "154377333799",
+  appId: "1:154377333799:web:b028f2037e5764b7527202"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(config);
